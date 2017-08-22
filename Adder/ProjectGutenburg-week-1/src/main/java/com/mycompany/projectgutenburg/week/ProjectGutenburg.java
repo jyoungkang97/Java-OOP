aad823356956869cf9e8ec41fcb2cfd03798e6d7
James Kang <Jyoungkang97@gmail.com>
2017-08-22 14:37:16 -0500

    changes

--- Adder/ProjectGutenburg-week-1/src/main/java/com/mycompany/projectgutenburg/week/ProjectGutenburg.java
+++ Adder/ProjectGutenburg-week-1/src/main/java/com/mycompany/projectgutenburg/week/ProjectGutenburg.java
@@ -11,6 +11,7 @@ package com.mycompany.projectgutenburg.week;
  */
 public class ProjectGutenburg {
     public static void main(String[] args) {
+        flakjfdjahl
         System.out.println("Did you know that in 1440 (or thereabouts)");
         System.out.println("Johannes Gutenberg invented the printing press?");
         System.out.println("He started out as a goldsmith!");
