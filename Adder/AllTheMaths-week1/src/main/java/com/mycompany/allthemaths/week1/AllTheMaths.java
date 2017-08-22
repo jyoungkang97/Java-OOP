644c0e3f25358f4ccb94a6339987e23645f823c0
James Kang <Jyoungkang97@gmail.com>
2017-08-22 15:16:53 -0500

    maths

---
+++ Adder/AllTheMaths-week1/src/main/java/com/mycompany/allthemaths/week1/AllTheMaths.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.allthemaths.week1;
+
+/**
+ *
+ * @author jyoun
+ */
+public class AllTheMaths {
+    public static void main(String[] args) {
+        System.out.println("1 + 2 is: ");
+        System.out.println(1 + 2);
+        
+        System.out.println("42001 modulus 5 is: ");
+        System.out.println(42001 % 5);
+        
+        System.out.println("5565.0 divided by 22.0 is : ");
+        System.out.println(5565.0 / 22.0);
+        
+        System.out.println("223 times 31 - 42: ");
+        System.out.println(233 * 31 - 42);
+
+        System.out.println("Is 4 greater than -1?");
+        System.out.println(4 > -1);
+        
+        System.out.println("\n****** Now make the computer do some harder maths!");
+        
+        System.out.println("8043.52 minus 4.2 plus 23.0 divided by 56.0 times -76.13 is: ");
+        System.out.println(8043.52 - 4.2 + 23.0 / (56.0 * -76.13));
+        
+        System.out.print("11111 modulus 3 minus 67 minus 1 plus 9 is: ");
+        System.out.println(11111 % 3 - 67 - 1 + 9);
+    }
+}
+
