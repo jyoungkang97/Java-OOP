af9d27570ff4c9a5d4ade97bec6d1923deeb6ab8
James Kang <Jyoungkang97@gmail.com>
2017-08-22 18:43:25 -0500

    First Menu

---
+++ Adder/MenuOfChampions/src/main/java/com/mycompany/menuofchampions/MenuOfChamp.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.menuofchampions;
+
+/**
+ *
+ * @author jyoun
+ */
+public class MenuOfChamp {
+    public static void main(String[] args) {
+        int tacos = 8;
+        int spaghetti = 12;
+        int donut = 4;
+        System.out.println("  ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( )\n" +
+                
+                           "-. X X X X X X X X X X X X X X X X X X X X X X X ,-\n" +
+                
+                           "  X X X X X X X X X X X X X X X X X X X X X X X X\n" +
+                
+                           " (_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_)");
+        
+        System.out.println("\n");
+        
+        System.out.println("             WELCOME TO JAMES' FAMILY RESTAURANT");
+        
+        System.out.println("\n");
+        
+        System.out.println("                The Menu for today is:");
+        
+        System.out.println("\n");
+        
+        System.out.println("         Beef Tacos " + "$" + tacos);
+        
+        System.out.println("         Spaghetti and Meatballs " + "$" + tacos);
+        
+        System.out.println("         Glazed Donut " + "$" + donut);
+        
+        System.out.println("\n");
+        
+        System.out.println("  ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( ( )\n" +
+                
+                           "-. X X X X X X X X X X X X X X X X X X X X X X X ,-\n" +
+                
+                           "  X X X X X X X X X X X X X X X X X X X X X X X X\n" +
+                
+                           " (_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_(_)");
+    }
+    
+}
