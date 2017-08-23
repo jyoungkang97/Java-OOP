0817d2056165318cbe3e22186e2d685bd7529cfa
James Kang <Jyoungkang97@gmail.com>
2017-08-23 12:48:56 -0500

    added

---
+++ Adder/LlamasWhales/src/main/java/com/mycompany/llamaswhales/LlamasWhales.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.llamaswhales;
+
+/**
+ *
+ * @author jyoun
+ */
+public class LlamasWhales {
+    public static void main(String[] args) {
+        int llamas = 20;
+        int whales = 15;
+        int dodos = 0;
+    
+    if (dodos > 0); {
+        System.out.println("I thought dodo's were extinct!");
+    }
+    
+    if (dodos < 0); {
+        System.out.println("Hold on, how can we have negative Dodos?");
+    }
+    
+    if (llamas > whales); {
+        System.out.println("Whales may be bigger, but we have more Llamas!");
+    }
+    
+    if (llamas <= whales); {
+        System.out.println("Qualiity over quantity, Whales are better.");
+    }
+    
+        System.out.println("Dodo's have been cloned, and are no longer extinct!");
+        
+        dodos += 100;
+    
+    if ((whales + llamas) < dodos) {
+        System.out.println("I never thought I'd see Dodo's rule the Earth.");
+    }
+    
+    if (llamas > whales && llamas > dodos) {
+        System.out.println("Llamas come out ahead, sneaky!");
+    }
+  }
+}
