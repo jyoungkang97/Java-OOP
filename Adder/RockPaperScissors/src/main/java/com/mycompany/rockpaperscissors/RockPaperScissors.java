5df2b6b1f03c337f1a0c0d02f8d098a288dcc66c
James Kang <Jyoungkang97@gmail.com>
2017-08-28 10:42:21 -0500

    new updates

--- Adder/RockPaperScissors/src/main/java/com/mycompany/rockpaperscissors/RockPaperScissors.java
+++ Adder/RockPaperScissors/src/main/java/com/mycompany/rockpaperscissors/RockPaperScissors.java
@@ -76,6 +76,8 @@ public class RockPaperScissors {
                 KeepPlaying = false;
             }
             roundsPlayed++;
+            
+            
         }
 
     }
--- Adder/SummativeSums/src/main/java/com/mycompany/summativesums/SummativeSums.java
+++ Adder/SummativeSums/src/main/java/com/mycompany/summativesums/SummativeSums.java
@@ -5,18 +5,37 @@
  */
 package com.mycompany.summativesums;
 
+import java.util.Arrays;
+
 /**
  *
  * @author jyoun
  */
 public class SummativeSums {
+
     public static void main(String[] args) {
-    int [] numbers =   { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
-    int [] numbers2 =    { 999, -60, -77, 14, 160, 301 };
-    int [] numbers3 =    { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
+
+        int[] numbers = {1, 90, -33, -55, 67, -16, 28, -55, 15};
+        int[] numbers2 = {999, -60, -77, 14, 160, 301};
+        int[] numbers3 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99};
+
+        int sum = 0;
+        for (int i = 0; i < 1; i++) {
+            sum += numbers[i];
+            System.out.println("#1 Array Sum: " + sum);
+        }
         
-        public static int addintegers (int numbers, int numbers2, int numbers3) {
-            System.out.println(numbers + numbers2 + numbers3);
+        int sum2 = 0;
+        for (int i = 0; i < numbers2.length; i++) {
+            sum2 += numbers2[i];
+            System.out.println("#2 Array Sum : " + sum2 );
+        }
+        
+        int sum3 = 0;
+        for (int i = 0; i < numbers3.length; i++) {
+            sum3 += numbers3[i];
+            System.out.println("#3 Array Sum : " + sum3);
         }
     }
 }
+
